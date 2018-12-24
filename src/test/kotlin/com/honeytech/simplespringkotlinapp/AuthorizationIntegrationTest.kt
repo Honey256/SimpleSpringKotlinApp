950759991d3f0a620f75f833c1b5f09368ff8e26
Honey256 <galkin.iliya.tver@gmail.com>
2018-12-24 10:33:15 +0300

    Добавил тест запроса авторизации (не рабочий)

--- src/test/kotlin/com/honeytech/simplespringkotlinapp/AuthorizationIntegrationTest.kt
+++ src/test/kotlin/com/honeytech/simplespringkotlinapp/AuthorizationIntegrationTest.kt
@@ -1,9 +1,10 @@
 package com.honeytech.simplespringkotlinapp
 
+import com.honeytech.simplespringkotlinapp.model.web.WebUser
 import org.junit.Assert
 import org.junit.Test
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
+import org.springframework.web.client.exchange
 import org.springframework.web.client.getForEntity
 
 
@@ -14,9 +15,20 @@ class AuthorizationIntegrationTest : AbstractIntegrationTest(){
     fun authorizationTest() {
 
         // проверка недоступности ресурса
-        val result : ResponseEntity<String> = restTemplate.getForEntity("http://localhost:8080/api/greeting?name=ilya", String::class, "Yana")
-        Assert.assertEquals(HttpStatus.UNAUTHORIZED, result.statusCode)
+        val unauthorizedResult : ResponseEntity<String> = restTemplate.getForEntity("http://localhost:8080/api/greeting?name=ilya", String::class, "Yana")
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, unauthorizedResult.statusCode)
 
+
+        val authorizationResult : ResponseEntity<WebUser> = restTemplate.exchange(
+                "http://localhost:8080/api/greeting?name=ilya",
+                HttpMethod.POST,
+                HttpEntity(WebUser::class),
+                WebUser("honey256","123q123"))
+        Assert.assertEquals(HttpStatus.OK, authorizationResult.statusCode)
+
+        //var header : HttpHeaders = result.headers
+        //var cookie = header.get(HttpHeaders.COOKIE)
+        //println(cookie)
 
     }
 }
